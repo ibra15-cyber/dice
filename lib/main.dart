43da62897f2df4df54f5bfbe506ab19854728352
ibra15-cyber <91777628+ibra15-cyber@users.noreply.github.com>
2021-12-02 15:49:19 +0000

    trying through git cmd

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +18,30 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //our app is in main but we are calling body only here
+    return Center(
+      child: Row(children: <Widget>[
+        //1st child
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              print("Left button pressed");
+            },
+            child: Image.asset("images/dice1.png"),
+          ),
+        ),
+
+        //2nd child
+        Expanded(
+          child: FlatButton(
+            //anonymous or arrow fn
+            onPressed: () {
+              print("Right button got pressed");
+            },
+            child: Image.asset("images/dice1.png"),
+          ),
+        ),
+      ]),
+    );
   }
 }
