53cd3adba72e2d1fbf4449f8656801cd5cf1b789
ibra15-cyber <91777628+ibra15-cyber@users.noreply.github.com>
2021-12-02 21:07:19 +0000

    first commit
    Signed-off-by: ibra15-cyber <91777628+ibra15-cyber@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
+//main as in java
 void main() {
+  //run an obj
   return runApp(
     MaterialApp(
       home: Scaffold(
@@ -9,13 +12,33 @@ void main() {
           title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
+
+        //our body its a class
         body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  //instance variable
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  //our methods
+  void changeDice (){
+    setState( () {
+      leftDiceNumber = Random().nextInt(6)  + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      // print("The dice is changed to $leftDiceNumber");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //our app is in main but we are calling body only here
@@ -23,22 +46,28 @@ class DicePage extends StatelessWidget {
       child: Row(children: <Widget>[
         //1st child
         Expanded(
-          child: FlatButton(
+          child: TextButton(
             onPressed: () {
-              print("Left button pressed");
-            },
-            child: Image.asset("images/dice1.png"),
+              changeDice();
+              },
+            child: Image.asset("images/dice$leftDiceNumber.png"),
           ),
         ),
 
         //2nd child
         Expanded(
-          child: FlatButton(
+          child: TextButton(
             //anonymous or arrow fn
             onPressed: () {
-              print("Right button got pressed");
-            },
-            child: Image.asset("images/dice1.png"),
+              changeDice();
+              // setState(() {
+              //   rightDiceNumber = Random().nextInt(6) + 1;
+              //   leftDiceNumber = Random().nextInt(6)  + 1;
+              //   // print("Right button got pressed $rightDiceNumber");
+              //
+              // });
+              },
+            child: Image.asset("images/dice$rightDiceNumber.png"),
           ),
         ),
       ]),
